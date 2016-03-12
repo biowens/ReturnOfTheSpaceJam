f5a69ddbbdb3c098fc6596b6d1981cae5670f8f4
Ann Ho <kjuri27@yahoo.com>
2016-03-12 01:16:49 -0500

    Added very basic director cam movement + vague item placement script

--- Assets/_Scripts/DirectorCamScript.cs
+++ Assets/_Scripts/DirectorCamScript.cs
@@ -8,8 +8,13 @@ public class DirectorCamScript : MonoBehaviour {
 	float zoomInCamSize;
 
 	float prevMouseX, prevMouseY;
-	float timeTilZoomOut;
+	public float timeTilZoomOut;
 	float lastMouseMoveTime;
+
+	public float mouseXConstraint, mouseYConstraint;
+	public float camPosXConstraint, camPosYConstraint;
+
+	public float zoomSpeed;
 
 	Camera directorCam;
 
@@ -19,29 +24,57 @@ public class DirectorCamScript : MonoBehaviour {
 		directorCam.orthographicSize = defaultCamSize;
 		zoomInCamSize = defaultCamSize / zoomInMult;
 
-		prevMouseX = Input.GetAxis ("Mouse X");
-		prevMouseY = Input.GetAxis ("Mouse Y");
+		prevMouseX = Input.mousePosition.x;
+		prevMouseY = Input.mousePosition.y;
+
+		lastMouseMoveTime = -10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetAxis ("Mouse X") != prevMouseX || Input.GetAxis ("Mouse Y") != prevMouseY) {
+		// Camera zoom control
+		if (Mathf.Abs(Input.mousePosition.x - prevMouseX) > 10f || Mathf.Abs(Input.mousePosition.y - prevMouseY) > 10f) {
 			lastMouseMoveTime = Time.timeSinceLevelLoad;
-			prevMouseX = Input.GetAxis ("Mouse X");
-			prevMouseY = Input.GetAxis ("Mouse Y");
+			prevMouseX = Input.mousePosition.x;
+			prevMouseY = Input.mousePosition.y;
 		}
 
-
-			
 		float timeSinceLastMove = Time.timeSinceLevelLoad - lastMouseMoveTime;
+		print (timeSinceLastMove);
 
-		if (timeSinceLastMove < 1f) {
-			lerpCamSize (defaultCamSize, zoomInCamSize, 0.01f);
+		// Zoom in if the mouse has been moved within a number of seconds
+		if (timeSinceLastMove < timeTilZoomOut) {
+			lerpCamSize (defaultCamSize, zoomInCamSize, zoomSpeed);
 		} else {
-			lerpCamSize (zoomInCamSize, defaultCamSize, 0.01f);
+			lerpCamSize (zoomInCamSize, defaultCamSize, zoomSpeed);
 		}
 
+		// Camera move controls
+		Vector3 screenPos = directorCam.WorldToScreenPoint(directorCam.ScreenToWorldPoint(Input.mousePosition));
+		float screenPosFromCenterX = (screenPos.x / directorCam.pixelWidth - 0.5f);
+		float screenPosFromCenterZ = (screenPos.y / directorCam.pixelHeight - 0.5f);
+
+		// move cam left/right
+		if (screenPosFromCenterX < -mouseXConstraint || screenPosFromCenterX > mouseXConstraint) {
+			// Treat camera move as mouse move
+			if (transform.position.x + (1f * screenPosFromCenterX) < camPosXConstraint
+			    && transform.position.x + (1f * screenPosFromCenterX) > -camPosXConstraint)
+				lastMouseMoveTime = Time.timeSinceLevelLoad;
+
+			float newXPos = Mathf.Clamp (transform.position.x + (1f * screenPosFromCenterX), -camPosXConstraint, camPosXConstraint);
+			transform.position = new Vector3(newXPos, transform.position.y, transform.position.z);
+		}
+		// move cam up/down
+		if (screenPosFromCenterZ < -mouseYConstraint || screenPosFromCenterZ > mouseYConstraint) {
+			// Treat camera move as mouse move
+			if (transform.position.z + (1f * screenPosFromCenterZ) < camPosYConstraint
+				&& transform.position.z + (1f * screenPosFromCenterZ) > -camPosYConstraint)
+				lastMouseMoveTime = Time.timeSinceLevelLoad;
+			
+			float newZPos = Mathf.Clamp (transform.position.z + (1f * screenPosFromCenterZ), -camPosYConstraint, camPosYConstraint);
+			transform.position = new Vector3(transform.position.x, transform.position.y, newZPos);
+		}
 	}
 
 	void lerpCamSize(float origSize, float finalSize, float speed) {
--- Assets/_Scripts/DirectorController.cs
+++ Assets/_Scripts/DirectorController.cs
@@ -17,23 +17,23 @@ public class DirectorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
 		// If player clicks
 		RaycastHit mouseRayHit;
 		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		if (Input.GetMouseButtonDown(0) && Physics.Raycast(mouseRay, out mouseRayHit)) {
+		if (Input.GetMouseButtonDown(0) 
+			&& Physics.Raycast(mouseRay.origin, mouseRay.direction, out mouseRayHit, Mathf.Infinity)) {
 			ghostObj = Instantiate<GameObject> (placeables [0]);
 			ghostObj.transform.position = mouseRayHit.point + new Vector3 (0, ghostObj.transform.localScale.y / 2f, 0);
 			hasGhostObj = true;
 		}
 		if (Input.GetMouseButton (0) && hasGhostObj) {
-
+			if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out mouseRayHit, Mathf.Infinity)) {
+				ghostObj.transform.position = new Vector3(mouseRayHit.point.x, ghostObj.transform.position.y, mouseRayHit.point.z);
+			}
 		}
-		if (Input.GetMouseButtonUp (0) && hasGhostObj) {
 
-			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
-			cube.transform.position = mouseRayHit.point;
+		if (Input.GetMouseButtonUp (0) && hasGhostObj) {
 
 			hasGhostObj = false;
 		}
